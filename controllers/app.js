b4b507b075ff6ae015f02a058d2007489a0f853a
Yuffie Hu <yuffieevans@gmail.com>
2022-09-01 10:53:57 -0500

    fix app.js route bugs

--- controllers/app.js
+++ controllers/app.js
@@ -1,8 +1,6 @@
-const { NOT_FOUND } = require('../helpers/utils');
-
 function getHomePage(req, res) {
-  res.status(NOT_FOUND);
+  res.status(404);
   res.send({ message: 'Requested resource not found' });
 }
 
-module.exports = getHomePage;
+module.exports = { getHomePage };
